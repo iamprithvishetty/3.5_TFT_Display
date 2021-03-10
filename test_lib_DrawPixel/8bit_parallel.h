146298dd2b59da283b419dda8c656ad73adf7709
iamprithvishetty <48802728+iamprithvishetty@users.noreply.github.com>
2021-03-10 15:27:45 +0530

    Add files via upload

--- test_lib_DrawPixel/8bit_parallel.h
+++ test_lib_DrawPixel/8bit_parallel.h
@@ -1,8 +1,8 @@
-#define RES_MASK 0x00001000 //define RES_MASK HEX VALUE here  RES : GPIO12
-#define CS_MASK 0x00000800  //define CS_MASK HEX VALUE here   CS : GPIO11
+#define RD_MASK 0x00001000 //define RD_MASK HEX VALUE here  RES : GPIO12
+#define WR_MASK 0x00000800  //define WR_MASK HEX VALUE here   CS : GPIO11
 #define CD_MASK 0x00000400  //define CD_MASK HEX VALUE here   CD : GPIO10
-#define WR_MASK 0x00000200  //define WR_MASK HEX VALUE here   WRITE : GPIO9
-#define RD_MASK 0x00000100  //define RD_MASK HEX VALUE here   READ : GPIO8
+#define CS_MASK 0x00000200  //define CS_MASK HEX VALUE here   WRITE : GPIO9
+#define RES_MASK 0x00000100  //define RES_MASK HEX VALUE here   READ : GPIO8
 
 #define CONTROL_MASK CS_MASK|CD_MASK|WR_MASK|RD_MASK|RES_MASK
 #define SET_CNTL_BITS write_word(GPIO_DIRECTION_CNTRL_REG, *GPIO_DIRECTION_CNTRL_REG | CONTROL_MASK)
@@ -24,7 +24,7 @@
 
 #define DATA_MASK 0x000000FF
 
-#define write8(d) { write_word(GPIO_DATA_REG,*GPIO_DATA_REG | (d & DATA_MASK)); WR_STROBE; }
+#define write8(d) { write_word(GPIO_DATA_REG,(*GPIO_DATA_REG&~DATA_MASK) | (d & DATA_MASK));WR_STROBE }
 #define read8(dst) { RD_ACTIVE;DELAY;dst=(read_word(GPIO_DATA_REG)&DATA_MASK);RD_IDLE; }
 
 #define write16(d) { uint8_t h = (d)>>8, l = d; write8(h); write8(l); }
@@ -34,8 +34,8 @@
 #define writeCmd16(x){ CD_COMMAND; write16(x); CD_DATA; }
 #define writeData16(x){ write16(x) }
 
-#define setWriteDir() write_word(GPIO_DIRECTION_CNTRL_REG,*GPIO_DIRECTION_CNTRL_REG|DATA_MASK)
-#define setReadDir() write_word(GPIO_DIRECTION_CNTRL_REG,*GPIO_DIRECTION_CNTRL_REG & ~DATA_MASK)
+#define setWriteDir() {write_word(GPIO_DIRECTION_CNTRL_REG,*GPIO_DIRECTION_CNTRL_REG|DATA_MASK);}
+#define setReadDir() {write_word(GPIO_DIRECTION_CNTRL_REG,*GPIO_DIRECTION_CNTRL_REG & ~DATA_MASK);}
 
 // Set value of TFT register: 8-bit address, 8-bit value
 #define writeCmdData8(a, d) { CD_COMMAND; write8(a); CD_DATA; write8(d); }
--- test_lib_DrawPixel/test_lib_DrawPixel.ino
+++ test_lib_DrawPixel/test_lib_DrawPixel.ino
@@ -1,55 +1,86 @@
 #include "8bit_parallel.h"
 
-#define ILI9341_COLADDRSET 0x2A
-#define ILI9341_PAGEADDRSET 0x2B
-#define ILI9341_MEMORYWRITE 0x2C
+#define XC 0x2A
+#define YC 0x2B
+#define CC 0x2C
 
-#define CC ILI9341_MEMORYWRITE
-#define XC ILI9341_COLADDRSET
-#define YC ILI9341_PAGEADDRSET
-#define TFTLCD_DELAY8 0x7F
+void setup() {
+  
+//RD_ACTIVE;
+//WR_ACTIVE;
+//CD_COMMAND;
+//CS_IDLE;
+//CD_DATA;
+//write8(0x00);
+//write8(0x00);
+//write8(0x00);
+//write8(0x00);
+//CD_DATA;
+//setWriteDir();
+//CS_IDLE;
 
-uint16_t dest;
-
-void setup() 
-{
-SET_CNTL_BITS; // SET Control Bits to OUTPUT
-RES_ACTIVE; // RESET pin HIGH    
-CS_ACTIVE;
-RD_IDLE;
-WR_IDLE;
-setWriteDir();
-//writeCmd8(0x00);
-writeCmd8(0x29);
-writeCmd8(0x2A);
-writeData16(20);
-writeData16(300);
-writeCmd8(0x2B);
-writeData16(20);
-writeData16(300);
-writeCmd8(0x2E);
+Init();
+delay(1000);
 }
 
 void loop() 
 {
-  read16(dest);
-  Serial.println(dest);
+ Serial.println(Read_Power());
+  for(int i=25;i<320;i++)
+  {
+    Draw_Pixe(50,i,0x0000);
+  }
+  delay(2000);
+}
+
+void Init()
+{
+ SET_CNTL_BITS;
+ CS_IDLE;
+ RD_IDLE;
+ WR_IDLE;
+ CD_DATA;
+ RES_ACTIVE;
+ setWriteDir();
+ //Serial.println(Read_ID());
+ Serial.println(Read_Power());
+ reset();
+ Start();
 
 }
 
-uint16_t Read_ID()
+void reset()
 {
-  uint16_t dst;
-  SET_CNTL_BITS; // SET Control Bits to OUTPUT
-  RES_ACTIVE; // RESET pin HIGH
-  setWriteDir(); 
-  writeCmd8(0xD3);
-  setReadDir();
-  read16(dst);
-  //Serial.println(dst); //To Debug
-  read16(dst);
-  //Serial.println(dst); //To Debug
-  return dst;
+  CS_IDLE;
+  RD_IDLE;
+  WR_IDLE;
+  RES_IDLE;
+  delay(2);
+  RES_ACTIVE;
+  CS_ACTIVE;
+  CD_COMMAND;
+  write8(0x00);
+  for(uint8_t i=0; i<3; i++)
+  {
+    WR_STROBE; // Three extra 0x00s
+  }
+  CS_IDLE;
+  
+}
+
+void Start()
+{
+  reset();
+  delay(200);
+  uint8_t data_buf[] = {0x55};
+  Push_Command(0x3A,data_buf,1);
+  uint8_t data_buf1[] = {};
+  Push_Command(0x11,data_buf,0);
+  uint8_t data_buf2[] = {0x28};
+  Push_Command(0x36,data_buf,1);
+  uint8_t data_buf3[] = {};
+  Push_Command(0x29,data_buf,0);
+  
 }
 
 void Push_Command(uint16_t cmd, uint8_t *block, int8_t N)
@@ -59,7 +90,7 @@ void Push_Command(uint16_t cmd, uint8_t *block, int8_t N)
     while (N-- > 0) 
   {
         uint8_t u8 = *block++;
-        writeData8(u8);
+        writeData8(u8); 
     }
     CS_IDLE;
 }
@@ -67,17 +98,19 @@ void Push_Command(uint16_t cmd, uint8_t *block, int8_t N)
 void Set_Addr_Window(int16_t x1, int16_t y1, int16_t x2, int16_t y2)
 {
   CS_ACTIVE;
-  uint8_t x_buf[] = {x1>>8,x1&0xFF,x2>>8,x2&0xFF}; 
-  uint8_t y_buf[] = {y1>>8,y1&0xFF,y2>>8,y2&0xFF}; 
-
-  Push_Command(XC, x_buf, 4); //set x address
-  Push_Command(YC, y_buf, 4); //set y address
+  
+    uint8_t x_buf[] = {x1>>8,x1&0xFF,x2>>8,x2&0xFF}; 
+    uint8_t y_buf[] = {y1>>8,y1&0xFF,y2>>8,y2&0xFF}; 
+  
+    Push_Command(XC, x_buf, 4); //set x address
+    Push_Command(YC, y_buf, 4); //set y address
+  
   CS_IDLE;    
 }
 
 void Draw_Pixe(int16_t x, int16_t y, uint16_t color)
 {
-  if((x < 0) || (y < 0) || (x > 480) || (y > 320))
+  if((x < 0) || (y < 0) || (x > 320) || (y > 480))
   {
     return;
   }
@@ -86,85 +119,43 @@ void Draw_Pixe(int16_t x, int16_t y, uint16_t color)
   writeCmdData16(CC, color);
   CS_IDLE;
 }
-void init_table8(const void *table, int16_t size)
+
+uint16_t Read_ID()
 {
-  uint8_t i;
-    uint8_t *p = (uint8_t *) table, dat[24];            //R61526 has GAMMA[22] 
-    while (size > 0) 
-  {
-        uint8_t cmd = pgm_read_byte(p++);
-        uint8_t len = pgm_read_byte(p++);
-        if (cmd == TFTLCD_DELAY8) 
-    {
-            delay(len);
-            len = 0;
-        } 
-    else 
-    {
-            for (i = 0; i < len; i++)
-            {
-                dat[i] = pgm_read_byte(p++);
-            }
-      Push_Command(cmd,dat,len);
-        }
-        size -= len + 2;
-    }
-}
- void Init_LCD(void)
-{ 
-  uint16_t lcd_model =0xFFFF;
-  if(lcd_model == 0xFFFF)
-  {
-    lcd_model = Read_ID(); 
-  }
-//  uint16_t ID = Read_ID(); 
-  start();
-//  Set_Rotation(0);
+  uint16_t dst;
+  CS_ACTIVE;
+  CD_COMMAND;
+  RD_IDLE;
+  WR_IDLE; 
+  write8(0xD3);
+  setReadDir();
+  CD_DATA;
+  read16(dst);
+  dst=0;
+  //Serial.println(dst); //To Debug
+  read16(dst);
+  setWriteDir();
+  CS_IDLE;
+  //Serial.println(dst); //To Debug
+  return dst;
 }
 
-void start()
+uint16_t Read_Power()
 {
-
-      //lcd_driver = ID_9486;
-      //WIDTH = 320,HEIGHT = 480;
-      //width = WIDTH, height = HEIGHT;
-      //XC=ILI9341_COLADDRSET,YC=ILI9341_PAGEADDRSET,CC=ILI9341_MEMORYWRITE,RC=HX8357_RAMRD,SC1=0x33,SC2=0x37,MD=ILI9341_MADCTL,VL=0,R24BIT=0;
-      static const uint8_t ILI9486_regValues[] PROGMEM = 
-      {
-        0xF1, 6, 0x36, 0x04, 0x00, 0x3C, 0x0F, 0x8F,
-        0xF2, 9, 0x18, 0xA3, 0x12, 0x02, 0xB2, 0x12, 0xFF, 0x10, 0x00, 
-        0xF8, 2, 0x21, 0x04,
-        0xF9, 2, 0x00, 0x08,
-        0x36, 1, 0x08, 
-        0xB4, 1, 0x00,
-        0xC1, 1, 0x41,
-        0xC5, 4, 0x00, 0x91, 0x80, 0x00,
-        0xE0, 15, 0x0F, 0x1F, 0x1C, 0x0C, 0x0F, 0x08, 0x48, 0x98, 0x37, 0x0A, 0x13, 0x04, 0x11, 0x0D, 0x00,
-        0xE1, 15, 0x0F, 0x32, 0x2E, 0x0B, 0x0D, 0x05, 0x47, 0x75, 0x37, 0x06, 0x10 ,0x03, 0x24, 0x20, 0x00,       
-        0x3A, 1, 0x55,
-        0x11,0,
-        0x36, 1, 0x28,
-        TFTLCD_DELAY8, 120,
-        0x29,0
-/*
-        0x01, 0,            //Soft Reset
-              TFTLCD_DELAY8, 150,  // .kbv will power up with ONLY reset, sleep out, display on
-              0x28, 0,            //Display Off
-              0x3A, 1, 0x55,      //Pixel read=565, write=565.
-              0xC0, 2, 0x0d, 0x0d,        //Power Control 1 [0E 0E]
-              0xC1, 2, 0x43, 0x00,        //Power Control 2 [43 00]
-              0xC2, 1, 0x00,      //Power Control 3 [33]
-              0xC5, 4, 0x00, 0x48, 0x00, 0x48,    //VCOM  Control 1 [00 40 00 40]
-              0xB4, 1, 0x00,      //Inversion Control [00]
-              0xB6, 3, 0x02, 0x02, 0x3B,  // Display Function Control [02 02 3B] 
-              0xE0, 15,0x0F, 0x24, 0x1C, 0x0A, 0x0F, 0x08, 0x43, 0x88, 0x32, 0x0F, 0x10, 0x06, 0x0F, 0x07, 0x00,
-              0xE1, 15,0x0F, 0x38, 0x30, 0x09, 0x0F, 0x0F, 0x4E, 0x77, 0x3C, 0x07, 0x10, 0x05, 0x23, 0x1B, 0x00,
-              0x11, 0,            //Sleep Out
-              TFTLCD_DELAY8, 150,
-              0x29, 0         //Display On
-*/
-      };
-      init_table8(ILI9486_regValues, sizeof(ILI9486_regValues));
-  
-  
+  uint8_t dst;
+  CS_ACTIVE;
+  CD_COMMAND;
+  RD_IDLE;
+  WR_IDLE; 
+  write8(0x0A);
+  setReadDir();
+  CD_DATA;
+  read8(dst);
+  dst=0;
+  //Serial.println(dst); //To Debug
+  read8(dst);
+  setWriteDir();
+  CS_IDLE;
+  //Serial.println(dst); //To Debug
+  return dst;
 }
